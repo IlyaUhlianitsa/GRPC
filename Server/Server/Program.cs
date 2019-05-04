15e3b235fb71febef70fa5eefdda07b903b9c809
Ilya Uglyanitsa <iuglyanitsa@elevatecredit.co.uk>
2019-05-04 12:56:01 +0300

    Initialize commit with grpc client-server application

---
+++ Server/Server/Program.cs
@@ -0,0 +1,33 @@
+﻿using Grpc.Core;
+using grpc_test.controllers;
+using System;
+
+namespace grpc_test
+{
+    class Program
+    {
+        const int Port = 50051;
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Server server = new Server
+                {
+                    Services = { AccountService.BindService(new AccountsImpl()) },
+                    Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+                };
+                server.Start();
+
+                Console.WriteLine("Accounts server listening on port " + Port);
+                Console.WriteLine("Press any key to stop the server...");
+                Console.ReadKey();
+
+                server.ShutdownAsync().Wait();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception encountered: {ex}");
+            }
+        }
+    }
+}
---
+++ Server/Server/controllers/AccountsImpl.cs
@@ -0,0 +1,16 @@
+﻿using Grpc.Core;
+using grpc_test.modules;
+using System.Threading.Tasks;
+
+namespace grpc_test.controllers
+{
+    class AccountsImpl : AccountService.AccountServiceBase
+    {
+        // Server side handler of the GetEmployeeName RPC
+        public override Task<EmployeeName> GetEmployeeName(EmployeeNameRequest request, ServerCallContext context)
+        {
+            EmployeeData empData = new EmployeeData();
+            return Task.FromResult(empData.GetEmployeeName(request));
+        }
+    }
+}
